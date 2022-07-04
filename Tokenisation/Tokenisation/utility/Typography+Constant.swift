f5cda162204ce8604fd15b27c01735167cc657aa
anandyn02vi <anand.kumar@ymedialabs.com>
2022-07-04 06:34:41 +0530

    files added

---
+++ Tokenisation/Tokenisation/utility/Typography+Constant.swift
@@ -0,0 +1,19 @@
+import UIKit
+extension Typography { 
+
+static let VOLTAIRENORMAL_REGULAR34 = Typography(
+familyName: "Voltaire",
+fontWeight: "Regular",
+fontSize: 34,
+lineHeight: 40.8,
+letterSpacing: -0.68
+)
+
+static let ARIALNORMAL_BOLD34 = Typography(
+familyName: "Arial",
+fontWeight: "Bold",
+fontSize: 34,
+lineHeight: 40.8,
+letterSpacing: -0.68
+) 
+}
---
+++ Tokenisation/Tokenisation/utility/UIColor+Constant.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+
+let YELLOW_COLOR = UIColor(named: "yellow")
+
+let BLUE3_COLOR = UIColor(named: "blue3")
+
+let BLUE2_COLOR = UIColor(named: "blue2")
+
+let BLUE1_COLOR = UIColor(named: "blue1")
+
+let GREEN2_COLOR = UIColor(named: "green2")
+
+let GREEN3_COLOR = UIColor(named: "green3")
+
+let GREEN1_COLOR = UIColor(named: "green1")
+
+let ORANGE_COLOR = UIColor(named: "orange")
+
+let RED_COLOR = UIColor(named: "red")
+
+let GRAY4_COLOR = UIColor(named: "gray4")
+
+let GRAY5_COLOR = UIColor(named: "gray5")
+
+let GRAY6_COLOR = UIColor(named: "gray6")
+
+let GRAY1_COLOR = UIColor(named: "gray1")
+
+let GRAY2_COLOR = UIColor(named: "gray2")
+
+let GRAY3_COLOR = UIColor(named: "gray3")
+
+let PURPLE1_COLOR = UIColor(named: "purple1")
+
+let PURPLE2_COLOR = UIColor(named: "purple2")
